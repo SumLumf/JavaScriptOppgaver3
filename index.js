10dae146047e617f2b9dc169e23c6aef2ada19ef
SumLumf <haagensenliam5@gmail.com>
2026-01-14 14:02:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+// Oppgave 1
+// Skriv ut «Hello World» på skjermen 10 ganger. 
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Hello World");
+// }   
+
+// Oppgave 2
+// Skriv JavaScript-koden som viser tallene fra 1-5. (1 og 5 er inkludert
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// Oppgave 3
+// Skriv JavaScript-koden som viser tallene fra 5-8. (5 og 8 er inkludert)
+
+// for (let i = 5; i <= 8; i++) {
+//     console.log(i);
+// }
+
+// Oppgave 4
+// Skriv JavaScript-koden som viser tallene fra 7-3. (7 og 3 er inkludert)
+
+// for (let i = 7; i >= 3; i--) {
+//     console.log(i);
+// }
+
+// Oppgave 5
+// Skriv JavaScript-koden som viser partallene fra 2-10. (2 og 10 er inkludert
+
+// for (let i = 2; i <= 10; i += 2) {
+//     console.log(i);
+// }
+
+// Oppgave 6
+// Skriv JavaScript-koden som viser oddetallene fra og med 9 til og med 1.  
+
+// for (let i = 9; i >= 1; i -= 2) {
+//     console.log(i);
+// }
+
+// Oppgave 7
+// Lag JS-program for å summere tallene mellom 1 og 4.  
+
+// let sum = 0;
+// for (let i = 1; i <= 4; i++) {
+//     sum += i;
+// }  
+// console.log(sum);
+
+// Oppgave 8
+// Lag en løkke som skriver ut følgende til konsollen: 
+
+// # 
+
+// ## 
+
+// ### 
+
+// #### 
+
+// ##### 
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row += "#";
+//     }
+//     console.log(row);
+// }
+
+// Oppgave 9
